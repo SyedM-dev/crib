d5f5dd21b07943bc7f467c011c8a682ef739c66d
Syed Daanish <me@syedm.dev>
2025-12-13 00:55:09 +0000

    Treat input thread as the non-blocking it is

--- src/main.cc
+++ src/main.cc
@@ -152,7 +152,8 @@ int main(int argc, char *argv[]) {
     std::this_thread::sleep_for(std::chrono::milliseconds(8));
   }
 
-  input_thread.detach();
+  if (input_thread.joinable())
+    input_thread.join();
 
   if (work_thread.joinable())
     work_thread.join();
